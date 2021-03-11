e9c3b20b54a7bd12f64d5d6e728372a559df909d
Justin Puzey <justpuzey@gmail.com>
2021-03-10 22:50:57 -0500

    add psudo-code and character arrays

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,20 @@
 // Assignment code here
+var specialChar = ["!", ",", "\”", "#", "$", "%", "&", "’", "(", ")", "*", "+", ",", "-", ".", ":", ";", "<", "=", ">", "?", "@", "[", "\\", "]", "^", "_", "`", "{", "|", "}", "~"];
+var lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var numeric = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
+var all = [];
+var password = ""
 
+//prompt for length of password. Loop until # of characters selected is between 8 and 128. 
+
+// propt for character types to include in password. 
+//add all characters from selected arrays to 'all' array
+//pull 1 random from each selected type and add to generated password
+
+
+// pull random characters from 'all' array until number of characters complete
+// display password
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
@@ -15,3 +30,6 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
+
